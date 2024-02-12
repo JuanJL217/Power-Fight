7e0f4f82c29df0fb46b0092da42b84380273f67a
Juan Juarez <juandocumentos217@gmail.com>
2024-02-11 22:15:54 -0300

    Mejoramiento de la estrctura con direcciones izqueirda y derecha, y diccionarios

--- Frames.py
+++ Frames.py
@@ -18,6 +18,12 @@ PNG_10 = '10.png'
 PNG_11 = '11.png'
 PNG_12 = '12.png'
 
+UNION = ' '
+
+LEFT = 'Left'
+RIGHT = 'Right'
+
+ACTION = "Action"
 IDLE = 'Idle'
 WALK = 'Walk'
 RUN = 'Run'
@@ -34,62 +40,132 @@ class Frame_Personajes:
     def __init__(self, nombre):
         self.nombre = nombre
         self.frames = {
-            IDLE: [],
-            WALK: [],
-            RUN: [],
-            JUMP: [],
-            SHIELD: [],
-            HURT: [],
-            ATAACK_1: [],
-            ATAACK_2: [],
-            ATAACK_3: [],
-            DEAD: []
+
+            # Movimientos a la izquierda
+            IDLE+UNION+LEFT: [],
+            WALK+UNION+LEFT: [],
+            RUN+UNION+LEFT: [],
+            JUMP+UNION+LEFT: [],
+            SHIELD+UNION+LEFT: [],
+            HURT+UNION+LEFT: [],
+            ATAACK_1+UNION+LEFT: [],
+            ATAACK_2+UNION+LEFT: [],
+            ATAACK_3+UNION+LEFT: [],
+            DEAD+UNION+LEFT: [],
+
+            # Movimientos a la derecha
+            IDLE+UNION+RIGHT: [],
+            WALK+UNION+RIGHT: [],
+            RUN+UNION+RIGHT: [],
+            JUMP+UNION+RIGHT: [],
+            SHIELD+UNION+RIGHT: [],
+            HURT+UNION+RIGHT: [],
+            ATAACK_1+UNION+RIGHT: [],
+            ATAACK_2+UNION+RIGHT: [],
+            ATAACK_3+UNION+RIGHT: [],
+            DEAD+UNION+RIGHT: [],
         }
     
-    def agregar_frame(self, accion, lista_frames):
-        self.frames[accion] = lista_frames
+    def agregar_frame(self, accion, movimiento):
+        self.frames[accion+UNION+LEFT] = movimiento[LEFT]
+        self.frames[accion+UNION+RIGHT] = movimiento[RIGHT]
 
     def obtener_frames(self, accion):
         return self.frames.get(accion, [])
     
     
-def ruta(nombre, accion, nombre_png):
+def ruta(nombre, accion, direccion, imagen_png):
     
-    return f"Personajes/{nombre}/{accion}/{nombre_png}"
+    return os.path.join("Personajes", nombre, accion, direccion, imagen_png)
 
 
-FRAMES_WALK_SAMURAI = [
-    ruta(SAMURAI, WALK, PNG_1),
-    ruta(SAMURAI, WALK, PNG_2),
-    ruta(SAMURAI, WALK, PNG_3),
-    ruta(SAMURAI, WALK, PNG_4),
-    ruta(SAMURAI, WALK, PNG_5),
-    ruta(SAMURAI, WALK, PNG_6),
-    ruta(SAMURAI, WALK, PNG_7),
-    ruta(SAMURAI, WALK, PNG_8)
-]
+FRAMES_WALK_SAMURAI = {
 
-FRAMES_SHIELD_SAMURAI = [
-    ruta(SAMURAI, SHIELD, PNG_1),
-    ruta(SAMURAI, SHIELD, PNG_2)
-]
+    ACTION: WALK,
 
-FRAMES_HURT_SAMURAI = [
-    ruta(SAMURAI, HURT, PNG_1),
-    ruta(SAMURAI, HURT, PNG_2)
-]
+    LEFT: [
+        ruta(SAMURAI, WALK, RIGHT, PNG_1),
+        ruta(SAMURAI, WALK, RIGHT, PNG_2),
+        ruta(SAMURAI, WALK, RIGHT, PNG_3),
+        ruta(SAMURAI, WALK, RIGHT, PNG_4),
+        ruta(SAMURAI, WALK, RIGHT, PNG_5),
+        ruta(SAMURAI, WALK, RIGHT, PNG_6),
+        ruta(SAMURAI, WALK, RIGHT, PNG_7),
+        ruta(SAMURAI, WALK, RIGHT, PNG_8)        
+    ],
 
-FRAMES_DEAD_SAMURAI = [
-    ruta(SAMURAI, DEAD, PNG_1),
-    ruta(SAMURAI, DEAD, PNG_2),
-    ruta(SAMURAI, DEAD, PNG_3)
-]
+    RIGHT: [
+        ruta(SAMURAI, WALK, RIGHT, PNG_1),
+        ruta(SAMURAI, WALK, RIGHT, PNG_2),
+        ruta(SAMURAI, WALK, RIGHT, PNG_3),
+        ruta(SAMURAI, WALK, RIGHT, PNG_4),
+        ruta(SAMURAI, WALK, RIGHT, PNG_5),
+        ruta(SAMURAI, WALK, RIGHT, PNG_6),
+        ruta(SAMURAI, WALK, RIGHT, PNG_7),
+        ruta(SAMURAI, WALK, RIGHT, PNG_8)
+    ]
+}
+
+
+FRAMES_SHIELD_SAMURAI = {
+
+    ACTION: SHIELD,
+
+    LEFT: [
+        ruta(SAMURAI, SHIELD, LEFT, PNG_1),
+        ruta(SAMURAI, SHIELD, LEFT, PNG_2)
+    ],
+
+    RIGHT: [
+        ruta(SAMURAI, SHIELD, RIGHT, PNG_1),
+        ruta(SAMURAI, SHIELD, RIGHT, PNG_2)
+    ]
+}
+
+
+FRAMES_HURT_SAMURAI = {
+
+    ACTION: HURT,
+
+    LEFT: [
+        ruta(SAMURAI, HURT, LEFT, PNG_1),
+        ruta(SAMURAI, HURT, LEFT, PNG_2)
+    ],
+
+    RIGHT: [
+        ruta(SAMURAI, HURT, RIGHT, PNG_1),
+        ruta(SAMURAI, HURT, RIGHT, PNG_2)
+    ]
+}
+
+
+FRAMES_DEAD_SAMURAI = {
+
+    ACTION: DEAD,
+
+    LEFT: [
+        ruta(SAMURAI, DEAD, LEFT, PNG_1),
+        ruta(SAMURAI, DEAD, LEFT, PNG_2),
+        ruta(SAMURAI, DEAD, LEFT, PNG_3)
+    ],
+
+    RIGHT: [
+        ruta(SAMURAI, DEAD, RIGHT, PNG_1),
+        ruta(SAMURAI, DEAD, RIGHT, PNG_2),
+        ruta(SAMURAI, DEAD, RIGHT, PNG_3)
+    ]
+}
+
 
 frames_samurai = Frame_Personajes(SAMURAI)
-frames_samurai.agregar_frame(WALK, FRAMES_WALK_SAMURAI)
-frames_samurai.agregar_frame(SHIELD, FRAMES_SHIELD_SAMURAI)
-frames_samurai.agregar_frame(HURT, FRAMES_HURT_SAMURAI)
-frames_samurai.agregar_frame(DEAD, FRAMES_DEAD_SAMURAI)
+# Hace ciclo for  
+frames_samurai.agregar_frame(FRAMES_WALK_SAMURAI)
+frames_samurai.agregar_frame(FRAMES_SHIELD_SAMURAI)
+frames_samurai.agregar_frame(FRAMES_HURT_SAMURAI)
+frames_samurai.agregar_frame(FRAMES_DEAD_SAMURAI)
+
+
+
 
 for i in range(1,4):
     imagen = Image.open(ruta(SAMURAI, DEAD, f"{i}.png"))
